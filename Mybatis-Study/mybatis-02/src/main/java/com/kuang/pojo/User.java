e7ee4f1cad069cf9a744384e8f965712217be9ad
MrYang02 <102336505+MrYang02@users.noreply.github.com>
2022-04-07 14:06:15 +0800

    Add files via upload

---
+++ Mybatis-Study/mybatis-02/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,53 @@
+package com.kuang.pojo;
+
+import org.apache.ibatis.type.Alias;
+
+//实体类
+@Alias("User")
+public class User {
+    private int uid;
+    private String uname;
+    private String uage;
+
+
+    public User() {
+    }
+
+    public User(int uid, String uname, String uage) {
+            this.uid = uid;
+            this.uname = uname;
+            this.uage = uage;
+        }
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUage() {
+        return uage;
+    }
+
+    public void setUage(String uage) {
+        this.uage = uage;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", uname='" + uname + '\'' +
+                ", uage='" + uage + '\'' +
+                '}';
+    }
+}
